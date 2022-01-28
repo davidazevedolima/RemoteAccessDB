1c32adf2cc6b5b1433b75790c790057042c8849b
davidazevedolima <davidazevedolima@gmail.com>
2022-01-28 00:55:11 +0000

    Added socket configuration through application.properties

--- src/main/java/com/ISTGRoup32/RemoteAccessDocument/RemoteAccessDocumentApplication.java
+++ src/main/java/com/ISTGRoup32/RemoteAccessDocument/RemoteAccessDocumentApplication.java
@@ -1,6 +1,7 @@
 package com.ISTGRoup32.RemoteAccessDocument;
 
 import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,21 +19,28 @@ import java.security.spec.InvalidKeySpecException;
 @SpringBootApplication
 public class RemoteAccessDocumentApplication {
 
+	private static int port;
+
+	@Value("${communication.port}")
+	public void setPort(int port) {
+		RemoteAccessDocumentApplication.port = port;
+	}
+
 	public static void main(String[] args) throws IOException, JSONException {
 		SpringApplication.run(RemoteAccessDocumentApplication.class, args);
 		System.out.println("Starting communications...");
 		Communication communication = null;
 		try {
-			communication = new Communication(58032);
+			communication = new Communication(port);
+			while (true) {
+				try {
+					communication.handleClient();
+				} catch (RuntimeException | NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException | UnrecoverableKeyException | NoSuchPaddingException | IllegalBlockSizeException | CertificateException | KeyStoreException | BadPaddingException e) {
+					System.out.println(e.getMessage());
+				}
+			}
 		} catch (UnrecoverableKeyException | CertificateException | KeyStoreException | NoSuchAlgorithmException e) {
 			throw new RuntimeException();
-		}
-		while (true) {
-			try {
-				communication.handleClient();
-			} catch (RuntimeException | NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException | UnrecoverableKeyException | NoSuchPaddingException | IllegalBlockSizeException | CertificateException | KeyStoreException | BadPaddingException e) {
-				System.out.println(e.getMessage());
-			}
 		}
 	}
 
